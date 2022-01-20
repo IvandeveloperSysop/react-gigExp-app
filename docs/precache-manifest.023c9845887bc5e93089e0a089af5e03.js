6e005eda13dcd6c48f0307790b060343892cba13
IvandeveloperSysop <70026601+IvandeveloperSysop@users.noreply.github.com>
2022-01-20 17:01:56 -0600

    Add files via upload

---
+++ docs/precache-manifest.023c9845887bc5e93089e0a089af5e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ee957aab32a87642f15142427d1ec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51d1297f74441e7a09f0",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d1297f74441e7a09f0",
+    "url": "/static/js/main.ae77ccd5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
